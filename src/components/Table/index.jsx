7d6e23fa67f28355469767c9c875bd0dc4ecf6ce
MalvinoTan <malvinoaustintanura@gmail.com>
2022-09-03 08:22:15 +0700

    Add thead & tbody to table component

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -4,25 +4,29 @@ import styles from "./style.module.css";
 const Table = ({ headers, datas }) => {
     return (
         <table className={styles.container}>
-            <tr>
+            <thead>
+                <tr>
+                    {
+                        headers.map((header, headerIdx) => {
+                            return <th key={headerIdx}>{header}</th>
+                        })
+                    }
+                </tr>
+            </thead>
+            <tbody>
                 {
-                    headers.map((header, headerIdx) => {
-                        return <th key={headerIdx}>{header}</th>
+                    Object.keys(datas).map((key, keyIdx) => {
+                        return (
+                            <tr key={keyIdx}>
+                                <td>{key}</td>
+                                <td>{(1.05 * datas[key]).toFixed(4)}</td>
+                                <td>{(1.0 * datas[key]).toFixed(4)}</td>
+                                <td>{(0.95 * datas[key]).toFixed(4)}</td>
+                            </tr>
+                        )
                     })
                 }
-            </tr>
-            {
-                Object.keys(datas).map((key, keyIdx) => {
-                    return (
-                        <tr key={keyIdx}>
-                            <td>{key}</td>
-                            <td>{(1.05 * datas[key]).toFixed(4)}</td>
-                            <td>{(1.0 * datas[key]).toFixed(4)}</td>
-                            <td>{(0.95 * datas[key]).toFixed(4)}</td>
-                        </tr>
-                    )
-                })
-            }
+            </tbody>
         </table>
     ); 
 };
